b7bc62b95a0a27ca4891b4bcc5c4597052e7ae4d
Kim-SiHwan <tjdans4166@naver.com>
2020-09-15 00:17:31 +0900

    BoardService

---
+++ src/main/java/restboard/restboard/service/BoardService.java
@@ -0,0 +1,56 @@
+package restboard.restboard.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import restboard.restboard.controller.BoardController;
+import restboard.restboard.domain.Board;
+import restboard.restboard.dto.Request;
+import restboard.restboard.dto.Response;
+import restboard.restboard.repository.BoardRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public Long uploadBoard(Request request){
+        Board board = request.toEntity(request);
+        boardRepository.save(board);
+        return board.getId();
+    }
+
+    @Transactional
+    public void deleteBoard(Long boardId){
+        Board board = boardRepository.findById(boardId).get();
+        boardRepository.delete(board);
+    }
+
+    @Transactional
+    public void updateBoard(Request request,Long boardId){
+        Board board = boardRepository.findById(boardId).get();
+        board.change(request.getTitle(), request.getContent(), request.getWriter());
+    }
+
+    public Response findById(Long boardId){
+        Board board = boardRepository.findById(boardId).get();
+        Response boardResponse = new Response(board);
+        return boardResponse;
+    }
+
+    public List<Response> findAll(){
+        List<Board> boardList = boardRepository.findAll();
+        List<Response> list = boardList.stream()
+                .map(m -> new Response(m))
+                .collect(Collectors.toList());
+        return list;
+    }
+
+
+}
