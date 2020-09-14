b6d03fded0f100f9d49531bb5a2c8ec9f45e3ce4
Kim-SiHwan <tjdans4166@naver.com>
2020-09-14 22:32:08 +0900

    Start

---
+++ src/main/java/restboard/restboard/RestboardApplication.java
@@ -0,0 +1,13 @@
+package restboard.restboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/restboard/restboard/RestboardApplicationTests.java
@@ -0,0 +1,13 @@
+package restboard.restboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
