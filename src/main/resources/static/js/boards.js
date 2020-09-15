222a022253456f522950206d594773b58deb1d11
Kim-SiHwan <tjdans4166@naver.com>
2020-09-15 13:54:21 +0900

    js

---
+++ src/main/resources/static/js/boards.js
@@ -0,0 +1,54 @@
+var boardManager = (function () {
+    var getAll = function (callback) {
+        $.ajax({
+            type: 'get',
+            url: '/boards/',
+            dataType: 'json',
+            success: callback
+        });
+    };
+
+    var getOne = function (obj,callback) {
+        console.log(obj);
+        console.log(callback);
+        $.ajax({
+            type:'get',
+            url: '/boards/'+obj.boardId,
+            dataType: 'json',
+            success : callback
+
+        });
+    };
+
+    var add = function (obj,callback) {
+        $.ajax({
+            type: 'post',
+            url: '/boards/',
+            data: JSON.stringify(obj),
+            contentType: "application/json",
+            dataType: 'json',
+            success: callback
+
+        });
+
+    };
+
+    var remove = function (obj, callback) {
+
+        $.ajax({
+            type: 'delete',
+            url: '/boards/' + obj.boardId,
+            dataType: 'json',
+            success: callback
+        });
+
+    };
+
+    return {
+        getAll: getAll,
+        getOne: getOne,
+        add: add,
+        remove: remove
+    }
+
+})();
