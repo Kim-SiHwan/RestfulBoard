50b36da59a7ca217799f471954c8138766b5f1cd
Kim-SiHwan <tjdans4166@naver.com>
2020-09-15 13:54:04 +0900

    Repository

--- src/main/java/restboard/restboard/repository/BoardRepository.java
+++ src/main/java/restboard/restboard/repository/BoardRepository.java
@@ -1,9 +1,14 @@
 package restboard.restboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import restboard.restboard.domain.Board;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Long> {
+    @Query("select b from Board b order by b.id desc ")
+    public List<Board> getBoardsDesc();
 }
